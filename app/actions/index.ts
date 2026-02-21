d3e5302c0ba64d453825623fffdcd77dbbda57e9
Tomás De Angelis <tomasde.angelis17@gmail.com>
2026-02-20 17:41:57 -0800

    Save

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,6 +1,7 @@
+"use server"
+
 import * as users from "./users"
 import * as sessions from "./sessions"
 import * as votes from "./votes"
-import * as utils from "./utils"
 
-export { users, sessions, votes, utils } 
+export { users, sessions, votes } 
